b2ba23a8e171f8ebb8318747a586136351ac4059
AbdullahRana22 <abullahr8@gmail.com>
2023-02-27 01:16:03 +0500

    firstcommit

---
+++ grade.js
@@ -0,0 +1,33 @@
+let percentage = prompt("Enter Your Percentage")
+
+if (percentage>=90){
+    alert ('Grade is A+')
+
+    console.log('Grade is A+')
+}
+else if (percentage>=80){
+    alert ('Grade is A1')
+
+    console.log('Grade is A1')
+}
+else if (percentage>=70){
+    alert ('Grade is A')
+
+    console.log('Grade is A')
+}
+else if (percentage>=60){
+    alert ('Grade is B')
+
+    console.log('Grade is B')
+}
+else if (percentage>=50){
+    alert ('Grade is C')
+
+    console.log('Grade is C')
+}
+else if (percentage>=0){
+    alert ('failed')
+
+    console.log ('failed')
+
+}
